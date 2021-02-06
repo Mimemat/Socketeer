e1e505c2d56f4847011cf5e7d8fcf4bd919d37ba
Mimemat <hyatomimemat@gmail.com>
2021-02-06 13:36:25 -0300

    test: add tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
+const { join } = require('path')
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 
 const { name } = require('./package.json');
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
-  clearMocks: true,
   displayName: name,
   name,
-  verbose: true,
   preset: 'ts-jest',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src/',
   }),
+  testMatch: [join(__dirname, 'tests/**/*.spec.{ts,tsx}')],
 };
---
+++ tests/test.spec.ts
@@ -0,0 +1,3 @@
+it('Should work', () => {
+  expect(1 + 1).toBe(2);
+});
